dd801152eacacbe06c8197cf0d8f830186510605
chulman <chlcjfals0122@gmail.com>
2020-04-12 18:36:02 +0900

    어노테이션 제거

--- cache/src/main/java/com/chulman/cache/redis/RedisCacheApplication.java
+++ cache/src/main/java/com/chulman/cache/redis/RedisCacheApplication.java
@@ -2,10 +2,8 @@ package com.chulman.cache.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = "com.chulman.cache.redis.cache")
 public class RedisCacheApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisCacheApplication.class, args);
