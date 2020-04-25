dcc96eb9b25afc0bcb27dada3584d532e2774042
chulman <chlcjfals0122@gmail.com>
2020-04-25 19:58:50 +0900

    add recipe 11-6

---
+++ batch/src/main/java/com/chulman/batch/recipe11_6/UserRegistrationService.java
@@ -0,0 +1,15 @@
+package com.chulman.batch.recipe11_6;
+
+import com.chulman.batch.UserRegistration;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+
+import java.util.Collection;
+import java.util.Date;
+
+public interface UserRegistrationService {
+
+    Collection<UserRegistration> getOutstandingUserRegistrationBatchForDate(int quantity, Date date);
+    @Retryable(backoff = @Backoff(delay = 1000, maxDelay = 10000, multiplier = 2))
+    UserRegistration registerUser(UserRegistration userRegistration);
+}
